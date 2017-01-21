dfa8345356399d3e462e0234e59ed921f2e24660
mynameis7 <andrew.rather5@gmail.com>
2017-01-21 00:27:15 -0500

    added other methods

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var gapi = require('./gapi.js');
 
 main.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
